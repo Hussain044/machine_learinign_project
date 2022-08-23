169e6ac2e81a97a97b5220e83666c3181e59009f
Hussain044 <mohdishtiaq044@gmail.com>
2022-08-24 00:00:26 +0200

    updated app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello, Working on my first End to End Project"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5000)
